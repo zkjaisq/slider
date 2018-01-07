48f1b8721022b6d22a068a06e5cb17f8e2549941
zkj <18771964684@163.com>
2018-01-07 18:09:45 +0800

    初步实现了无缝轮播，还有一个BUG

--- pingguo/pingguo.js
+++ pingguo/pingguo.js
@@ -1,18 +1,59 @@
 let $allList = $('#foot li') 
-console.log($allList)
+let $allImages =$('#images').children('img')
+let $firstCopy =$allImages.eq(0).clone(true)
+let $lastCopy =$allImages.eq($allImages.length - 1).clone(true)
+$('#images').append($firstCopy)
+$('#images').prepend($lastCopy)
+
+$('#images').css({transform:'translateX(-920px)'})
+
+let current =0
 $allList.eq(0).on('click',function(){
-    $('#view').css({transform:'translateX(0)'})
-    $allList.eq(0).addClass('active')
+    if(current===3){
+        $allList.removeClass('active')
+        $allList.eq(3).addClass('active')
+        $('#images').css({transform:'translateX(-4600px)'})
+        .one('transitionend',function(){
+            $('#images').hide().offset()
+            $('#images').css({transform:'translateX(-920px)'})
+            $('#images').show()
+        })
+
+    }else{
+        $('#images').css({transform:'translateX(-920px)'});
+        $allList.removeClass('active')
+        $allList.eq(0).addClass('active')
+        current=0
+    }
+    
 })
 $allList.eq(1).on('click',function(){
-    $('#view').css({transform:'translateX(920)'})
+    $('#images').css({transform:'translateX(-1840px)'});
+    $allList.removeClass('active')
     $allList.eq(1).addClass('active')
+    current =1
 })
 $allList.eq(2).on('click',function(){
-    $('#view').css({transform:'translateX(1840)'})
+    $('#images').css({transform:'translateX(-2760px)'});
+    $allList.removeClass('active')
     $allList.eq(2).addClass('active')
+    current=2
 })
 $allList.eq(3).on('click',function(){
-    $('#view').css({transform:'translateX(2700)'})
-    $allList.eq(3).addClass('active')
+    if(current==0){
+        $allList.removeClass('active')
+        $allList.eq(3).addClass('active')
+        $('#images').css({transform:'translateX(0px)'})
+        .one('transitionend',function(){
+            $('#images').hide().offset()
+            $('#images').css({transform:'translateX(-3680px)'})
+            $('#images').show()
+        })
+    }else{
+        $('#images').css({transform:'translateX(-3680px)'});
+        $allList.removeClass('active')
+        $allList.eq(3).addClass('active')
+        current=3
+    }
+    
 })
